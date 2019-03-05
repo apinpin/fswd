72fa42256ace69508b11cefdb5ca046ee34acd0e
Andro <andropinpin@gmail.com>
2019-03-04 22:37:44 -0800

    Simple todo list

---
+++ list.js
@@ -0,0 +1,21 @@
+window.setTimeout(function() {
+    // put all of your JS code from the lecture here
+    var todos = ["Buy New Turtle"];
+
+    var input = prompt("What would you like to do?");
+
+    while(input !== "quit")
+    {
+        if(input === "list"){
+            console.log(todos);
+        }
+        else if(input === "new"){
+            var newTodo = prompt("Enter new todo");
+            todos.push(newTodo);
+        }
+
+        input = prompt("What would you like to do?");
+    }
+
+    console.log("OK, YOU QUIT THE APP");
+  }, 500);
